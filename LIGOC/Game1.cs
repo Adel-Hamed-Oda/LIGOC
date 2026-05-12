aeb8e1505a566f9b3ada07d29a27664b082279e1
Adel Hamed <adel.hamed.oda@gmail.com>
2026-05-12 15:23:38 +0300

    MA3AMALTESH HAGA

--- LIGOC/Game1.cs
+++ LIGOC/Game1.cs
@@ -18,24 +18,18 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        // TODO: use this.Content to load your game content here
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
-        // TODO: Add your update logic here
 
         base.Update(gameTime);
     }
--- LIGOC/obj/Debug/net9.0/LIGOC.AssemblyInfo.cs
+++ LIGOC/obj/Debug/net9.0/LIGOC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LIGOC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+351bf6a0a74cb2ad8c91a0d9ca62ed0569eee1dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e16ae0a6301ef8b55131743931131e7167a2092d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LIGOC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LIGOC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
