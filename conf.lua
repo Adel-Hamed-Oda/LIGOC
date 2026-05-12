b40a416a77a3121b03c828769d5fb319f90ada13
Adel Hamed <adel.hamed.oda@gmail.com>
2026-05-12 14:11:55 +0300

    removed all to restart using the MonoEngine framework

--- conf.lua
+++
@@ -1,9 +0,0 @@
----@diagnostic disable: undefined-global
-function love.conf(t)
-    t.identity = "LIGOC"
-    t.window.title = "Logic Gates"
-    t.window.width = 1280
-    t.window.height = 720
-    t.window.vsync = 1
-    t.window.resizable = true
-end
--- run-game.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"%~dp0tools\love2d\love.exe" "%~dp0."
